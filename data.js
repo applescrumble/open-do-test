b36a395c114358b25caf0b840af166ef16a09e2c
applescrumble <applescrumble05@gmail.com>
2026-02-11 10:55:33 +0900

    画像を.webpに

--- data.js
+++ data.js
@@ -3,36 +3,36 @@ export const appList = [
     title: "九九カード", 
     desc: "",
     link: "https://view.genially.com/6959d78868f005afdae6b02b/interactive-content-", // 実際のURLに
-    image: "images/kuku_card.png" // 仮の画像URL
+    image: "images/kuku_card.webp" // 仮の画像URL
   },
   { 
     title: "九九カード②", 
     desc: "",
     link: "https://view.genially.com/6959bedbaf1831ae6ecb3a8a/interactive-content-", // 実際のURLに
-    image: "images/kuku_card2.png" // 仮の画像URL
+    image: "images/kuku_card2.webp" // 仮の画像URL
   },
   { 
     title: "好きなもの紹介カード", 
     desc: "",
     link: "https://view.genially.com/695a26ebaf1831ae6edde7ad/interactive-content-like", // 実際のURLに
-    image: "images/iLike_card.png" // 仮の画像URL
+    image: "images/iLike_card.webp" // 仮の画像URL
   },
   { 
     title: "小文字カード", 
     desc: "",
     link: "https://view.genially.com/695aff1b68f005afda1be0a1/interactive-content-", // 実際のURLに
-    image: "images/lower_card.png" // 仮の画像URL
+    image: "images/lower_card.webp" // 仮の画像URL
   },
   { 
     title: "大文字カード", 
     desc: "",
     link: "https://view.genially.com/695b63410205f1624b6e5dfc/interactive-content-", // 実際のURLに
-    image: "images/upper_card.png" // 仮の画像URL
+    image: "images/upper_card.webp" // 仮の画像URL
   },
   { 
     title: "並べ替えカード", 
     desc: "",
     link: "https://view.genially.com/69880744dea8f5798eae110d/interactive-content-",
-    image: "images/change_card.png"
+    image: "images/change_card.webp"
   }
 ];
