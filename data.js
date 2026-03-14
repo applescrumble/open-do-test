12660cd515613281cc2083ed83c5776593b3b30c
applescrumble <applescrumble05@gmail.com>
2026-03-14 11:00:45 +0900

    フォルダ構造を改善

--- data.js
+++ data.js
@@ -3,54 +3,63 @@ export const appList = [
     title: "九九カード", 
     desc: "",
     link: "https://view.genially.com/6959d78868f005afdae6b02b/interactive-content-", // 実際のURLに
-    image: "images/kuku_card.webp" // 仮の画像URL
+    image: "materials/material_01/thumb.webp" // 仮の画像URL
   },
   { 
     title: "九九カード②", 
     desc: "",
     link: "https://view.genially.com/6959bedbaf1831ae6ecb3a8a/interactive-content-", // 実際のURLに
-    image: "images/kuku_card2.webp" // 仮の画像URL
+    image: "materials/material_02/thumb.webp" // 仮の画像URL
   },
   { 
     title: "好きなもの紹介カード", 
     desc: "",
     link: "https://view.genially.com/695a26ebaf1831ae6edde7ad/interactive-content-like", // 実際のURLに
-    image: "images/iLike_card.webp" // 仮の画像URL
+    image: "materials/material_03/thumb.webp" // 仮の画像URL
   },
   { 
     title: "小文字カード", 
     desc: "",
     link: "https://view.genially.com/695aff1b68f005afda1be0a1/interactive-content-", // 実際のURLに
-    image: "images/lower_card.webp" // 仮の画像URL
+    image: "materials/material_04/thumb.webp" // 仮の画像URL
   },
   { 
     title: "大文字カード", 
     desc: "",
     link: "https://view.genially.com/695b63410205f1624b6e5dfc/interactive-content-", // 実際のURLに
-    image: "images/upper_card.webp" // 仮の画像URL
+    image: "materials/material_05/thumb.webp" // 仮の画像URL
   },
   { 
     title: "並べ替えカード", 
     desc: "",
     link: "https://view.genially.com/69880744dea8f5798eae110d/interactive-content-",
-    image: "images/change_card.webp"
+    image: "materials/material_06/thumb.webp"
   },
   { 
     title: "アルファベットカード", 
     desc: "",
-    link: "materials/alphabetCards",
-    image: "images/alphabet_card.webp"
+    link: "materials/material_07",
+    image: "materials/material_07/thumb.webp"
   },
   { 
     title: "アルファベットビンゴ", 
     desc: "",
-    link: "materials/alphabetBingo",
-    image: "images/alphabet_bingo.webp"
+    link: "materials/material_08",
+    image: "materials/material_08/thumb.webp"
   },
   { 
     title: "並べ替えカード②", 
     desc: "",
-    link: "materials/dragCards",
-    image: "images/drag_card.webp"
+    link: "materials/material_09",
+    image: "materials/material_09/thumb.webp"
   }
 ];
+/*
+,
+  { 
+    title: "", 
+    desc: "",
+    link: "materials/material_00",
+    image: "materials/material_00/thumb.webp"
+  }
+*/
