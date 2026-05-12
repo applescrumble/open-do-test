d811c29faff97e70e104db072a6b71acb059bd07
applescrumble <applescrumble05@gmail.com>
2026-05-12 13:12:38 +0900

    htmlで筆算を表現するためのグリッドを用意した

--- materials/material_13/script2.js
+++ materials/material_13/script2.js
@@ -1,5 +1,4 @@
 //a÷b=c...d
-
 const a=125;
 const b=5;
 let str=a.toString();
@@ -8,20 +7,33 @@ let amari=0;
 let target=0;
 let result="";
 
+function putDigit(row, col, value){
+    const div=document.createElement('div');
+    div.className='cell';
+    div.textContent=value;
+
+    div.style.gridRow=row;
+    div.style.gridColumn=col;
+
+    document.getElementById('hissan-container').appendChild(div);
+}
+
+
+
+
+
+
 for(let digit of str){
     let value=Number(digit);
-
     target=amari*10+value;
-
     sho=target/b|0;
     amari=target%b;
-    
     if(sho===0&&result===""){
         result="";
     }else{
         result+=sho.toString();
     }
-    
     console.log(`注目中の数字: ${digit}, 計算対象: ${target}, 商: ${sho}, 余り: ${amari}`);
 }
+
 console.log(result+" あまり"+amari);
