aefbb76d6de891f7eadb71abcfd5b3d5fbe46709
applescrumble <applescrumble05@gmail.com>
2026-05-12 12:52:25 +0900

    商の最大の位が0にならないようにした

--- materials/material_13/script2.js
+++ materials/material_13/script2.js
@@ -1,7 +1,7 @@
 //a÷b=c...d
 
-const a=86;
-const b=7;
+const a=125;
+const b=5;
 let str=a.toString();
 let sho=0;
 let amari=0;
@@ -13,11 +13,15 @@ for(let digit of str){
 
     target=amari*10+value;
 
-
     sho=target/b|0;
     amari=target%b;
     
-    result+=sho.toString();
+    if(sho===0&&result===""){
+        result="";
+    }else{
+        result+=sho.toString();
+    }
+    
     console.log(`注目中の数字: ${digit}, 計算対象: ${target}, 商: ${sho}, 余り: ${amari}`);
 }
 console.log(result+" あまり"+amari);
