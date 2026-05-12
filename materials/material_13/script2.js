ac49594e156d4cf9f9a2c653b3e42ee3cebce6a5
applescrumble <applescrumble05@gmail.com>
2026-05-12 15:32:03 +0900

    基本の筆算完成

--- materials/material_13/script2.js
+++ materials/material_13/script2.js
@@ -7,6 +7,8 @@ let amari=0;
 let target=0;
 let result="";
 
+let steps=[];
+
 function putDigit(row, col, value){
     const div=document.createElement('div');
     div.className='cell';
@@ -21,7 +23,6 @@ function putDigit(row, col, value){
 
 putDigit(2,1,b);
 putDigit(2,2,")");
-
 
 let i=0;
 for(let digit of str){
@@ -40,23 +41,37 @@ for(let digit of str){
         result+=sho.toString();
     }
 
-    putDigit(2, 3+i, digit);
-    putDigit(1, 3+i, sho);
-
-    strSho=sho.toString();
-    let j=0;
-    for(let digit2 of strSho){
-        putDigit(3+i*2, 3+i+j, digit2*b);
-    }
-
-    //putDigit(3+i*2, 3+i, sho*b);
-    putDigit(4+i*2, 3+i, digit-sho*b);
-
-
-
-    console.log(`注目中の数字: ${digit}, 計算対象: ${target}, 商: ${sho}, 余り: ${amari}`);
+    //console.log(`注目中の数字: ${digit}, 計算対象: ${target}, 商: ${sho}, 余り: ${amari}`);
     
+    steps.push({
+        index: i,
+        target: target,
+        sho: sho,
+        sub: sho*b,
+        amari: target%b
+    });
+console.log(steps[i]);
     i++;
 }
 
 console.log(result+" あまり"+amari);
+
+steps.forEach((step, i)=> {
+    putDigit(2, 3+i, str[i]);//割られる数
+
+    if(step.index>0){
+        putDigit(2+step.index*2, 3+step.index, str[step.index]);//割られる数を下ろしてくる
+    }
+
+    if(step.sho>0||result!==""){
+        putDigit(1, 3+step.index, step.sho);//商
+        
+        let subStr=step.sub.toString();
+        for(let j=0; j<subStr.length; j++){
+            putDigit(3+step.index*2, 3+step.index-(subStr.length-1)+j,subStr[j]);//引く数
+            
+        }
+        putDigit(4+step.index*2, 3+step.index, step.amari);
+    };
+    
+});
